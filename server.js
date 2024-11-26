3183666ef8689050140ef3e6a031858a2e67d404
Taylon <116691365+Lokyznx@users.noreply.github.com>
2024-11-25 23:57:34 -0300

    update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
+import routes from "./src/routes/postsRoutes.js";
 
+// Cria uma instância do Express, que será nosso servidor web
 const app = express();
+routes(app)
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console
 app.listen(3000, () => {
   console.log('Servidor escutando...');
-});
-
-app.get("/api", (req, res) => {
-    res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
 });
