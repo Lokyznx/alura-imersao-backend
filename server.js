c61acb226b9c92461c86f68bb5adbeba7a8caac9
Taylon <116691365+Lokyznx@users.noreply.github.com>
2024-11-26 21:33:35 -0300

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import conectarAoBanco from "./src/config/dbConfig.js";
 import routes from "./src/routes/postsRoutes.js";
-
 // Cria uma instância do Express, que será nosso servidor web
 const app = express();
+app.use(express.static("uplouds"))
 routes(app)
 // Inicia o servidor na porta 3000 e exibe uma mensagem no console
 app.listen(3000, () => {
   console.log('Servidor escutando...');
-});
+})
